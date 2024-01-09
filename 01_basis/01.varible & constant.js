1af287a58045a713b445a8dc49a93283c1946fa0
vivek mainali <vivekmainali3@gmail.com>
2024-01-09 08:22:52 +0545

    second commit

---
+++ 01_basis/01.varible & constant.js
@@ -0,0 +1,7 @@
+const account_Number = 12134567878
+let account_Name= "vivek"
+var account_city= "kathmandu"
+accountstate = "nepal"
+let account_portfolio;
+
+console.table([account_Number, account_Name, account_city, accountstate,account_portfolio]);
