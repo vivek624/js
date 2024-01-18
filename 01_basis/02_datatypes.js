c4c4a42ff21b96066c7e47ae66ab848057362699
vivek mainali <vivekmainali3@gmail.com>
2024-01-18 18:37:58 +0545

    conversionofdatatypes

---
+++ 01_basis/02_datatypes.js
@@ -0,0 +1,14 @@
+// let score = "false"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = String(score);
+
+// console.log(typeof valueInNumber);
+// console.log(Number(valueInNumber));
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
